44a8a71eed1cd0d9e12e4071d7222d5a5b394141
junvely <junvely97@gmail.com>
2024-03-21 02:03:35 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
 export default async function Page() {
-  console.log(process.env.SERVER_URL);
-  const res = await fetch(`${process.env.SERVER_URL}/api/test`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    next: { revalidate: 60 },
-  });
-  const { data } = await res.json();
-  console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
-  console.log(data);
+  //   const res = await fetch(`${process.env.SERVER_URL}/api/test`, {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     next: { revalidate: 60 },
+  //   });
+  //   const { data } = await res.json();
+  //   console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+  //   console.log(data);
   return (
     <div>
       <h1>HELLOOOOO!!!!</h1>
-      {data.map((post) => (
+      {/* {data.map((post) => (
         <div key={post.id}>
           <p>{post.title}</p>
           <p>{post.content}</p>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
