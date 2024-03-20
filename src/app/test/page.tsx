277dc66c857bcab79380989e3c21620d6f7d7814
junvely <junvely97@gmail.com>
2024-03-21 02:16:34 +0900

    test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,16 @@
+import { getPost } from "../../service/postApi";
+
+export default async function Page() {
+  const data = await getPost();
+  return (
+    <div>
+      <h1>TESTTTTTTTTTTT!!!!</h1>
+      {data?.map((post) => (
+        <div key={post.id}>
+          <p>{post.title}</p>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
